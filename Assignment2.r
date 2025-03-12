68b475025e1ccd464063eb219f405b72b6fbbf83
Mxxxviii <micyoung@claflin.edu>
2025-03-12 12:21:12 -0400

    Add files via upload

---
+++ Assignment2.r
@@ -0,0 +1,10 @@
+Pearson's product-moment correlation
+
+data:  my_data$cyl and my_data$wt
+t = 6.8833, df = 30, p-value = 1.218e-07
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ 0.5965795 0.8887052
+sample estimates:
+      cor 
+0.7824958 
